6490a3e1c5dea8bfc6d4b4e1b801b2045f8954c0
Mike Nanto <mike@thecowboymechanic.com>
2021-01-03 21:11:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,10 @@
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
-  
+  attendees.map do |attendees|
+    
 end
