d257843c657356662a85224218680533d9f21dfc
Mike Nanto <mike@thecowboymechanic.com>
2021-01-03 21:21:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,4 +14,5 @@ def assign_rooms(attendees)
   room = [1,2,3,4,5,6,7]
   attendees.each_with_index.map do |attendees, index|
     "Hello, #{attendee}! You'll be assigned to room #{index+1}"
+  end
 end
