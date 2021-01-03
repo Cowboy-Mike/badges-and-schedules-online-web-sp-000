5fcab57fc2dd7d74997ab5c35b5f237e470d552c
Mike Nanto <mike@thecowboymechanic.com>
2021-01-03 21:28:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,6 +16,12 @@ def assign_rooms(attendees)
   end
 end
 
-def printer
-  puts batch_badge_creator
-  puts assign_rooms(attendees).ea
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end
